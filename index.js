b53e29f7cd3943b9421c370a566237e34840eaac
almahmudzihad <amzihad@gmail.com>
2026-05-18 22:11:03 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+     await client.connect();
+    const database = client.db("studynook");
+    const collection = database.collection("rooms");
+
+    app.get('/rooms', async (req, res) => {
+        const result = await collection.find({}).toArray();
+        res.send(result);
+    })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => 
