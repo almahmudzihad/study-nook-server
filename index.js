702706b8eca8c09226ed1aaafeb6f53f65421a24
almahmudzihad <amzihad@gmail.com>
2026-05-19 00:55:45 +0600

    single id get route

--- index.js
+++ index.js
@@ -12,36 +12,70 @@ app.use(express.json());
 const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-     await client.connect();
-    const database = client.db("studynook");
-    const collection = database.collection("rooms");
+    try {
+        await client.connect();
+        const db = client.db("studynook");
 
-    app.get('/rooms', async (req, res) => {
-        const result = await collection.find({}).toArray();
-        res.send(result);
-    })
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        app.post("/rooms", async (req, res) => {
+            const room = req.body;
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+            const result = await db.collection("rooms").insertOne(room);
+
+            res.send(result);
+        });
+
+        app.get("/rooms", async (req, res) => {
+            try {
+                const rooms = await db
+                    .collection("rooms")
+                    .find()
+                    .sort({ createdAt: -1 })
+                    .toArray();
+
+                res.send(rooms);
+            } catch (error) {
+                res.status(500).send({
+                    message: error.message,
+                });
+            }
+        });
+        app.get("/rooms/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+
+                const room = await db.collection("rooms").findOne({
+                    _id: new ObjectId(id),
+                });
+
+                res.send(room);
+            } catch (error) {
+                res.status(500).send({
+                    message: error.message,
+                });
+            }
+        });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res) => 
+app.get('/', (req, res) =>
     res.send('studynook Server is running!')
 );
 
