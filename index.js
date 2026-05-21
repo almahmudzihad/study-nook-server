e240d05a3d8e4eb649c0ddd16d28b75b51fe399a
almahmudzihad <amzihad@gmail.com>
2026-05-21 15:00:11 +0600

    for deploy

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        //await client.connect();
         const db = client.db("studynook");
         const bookingsCollection = db.collection("bookings");
 
